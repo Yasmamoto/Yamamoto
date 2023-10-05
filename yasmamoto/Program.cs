744de71be52d0c0b532f5906a3542db3045fb6e2
Yasmamoto <yamamotoyato31@gmail.com>
2023-10-05 15:47:29 -0400

    msg do commit

--- yasmamoto/Program.cs
+++ yasmamoto/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net.WebSockets;
+
+class Yasmamoto
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite um numero: ");
+        double num = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digit outro numero: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        double result = num + num2;
+
+        Console.WriteLine($"As somas dos numeros é igual a {result}");
+
+        if(num2 > num)
+        {
+            Console.WriteLine($"O numero {num2} é maior");
+        }
+
+        Console.ReadKey();
+    }
+}
